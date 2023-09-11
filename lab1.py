b962f48030402bba3eebef0080a802770bd6e62d
Favournwa <nwachukw@ualberta.ca>
2023-09-10 22:20:05 -0600

    prints source code

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 import os
+
 import requests
 
 # Check the version of requests installed
@@ -10,3 +11,7 @@ url = "http://www.google.com/"
 
 # A GET request to the API
 response = requests.get(url)
+
+r = requests.get('https://raw.githubusercontent.com/Favournwa/CMPUT_404_lab/main/lab1.py')
+
+print(r.text)
