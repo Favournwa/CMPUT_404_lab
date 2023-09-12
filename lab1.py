e7530459de1c1f08a179dbdd4660cd3f98a6c6cb
Favournwa <91228828+Favournwa@users.noreply.github.com>
2023-09-11 23:08:18 -0600

    Update lab1.py
    added comments

--- lab1.py
+++ lab1.py
@@ -9,9 +9,10 @@ os.system(cmd)
 # The API endpoint
 url = "http://www.google.com/"
 
-# A GET request to the API
+# A GET request to Google
 response = requests.get(url)
 
+# A GET request to the python code 
 r = requests.get('https://raw.githubusercontent.com/Favournwa/CMPUT_404_lab/main/lab1.py')
 
 print(r.text)
